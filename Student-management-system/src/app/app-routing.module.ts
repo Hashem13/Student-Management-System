09c1b90d5f8e95ac704241dab4635c36581ecb1b
Yusuf Hashem <77444458+Hashem13@users.noreply.github.com>
2024-09-07 23:19:46 +0300

    update

--- Student-management-system/src/app/app-routing.module.ts
+++ Student-management-system/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { ScheduleListComponent } from './components/schedule-list/schedule-list.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginFormComponent },
+  { path: 'courses', component: CourseListComponent },
+  { path: 'registration', component: RegisterFormComponent },
+  { path: 'schedule', component: ScheduleListComponent },
+  // Add other routes or wildcard route here if necessary
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- Student-management-system/src/app/app.module.ts
+++ Student-management-system/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { ScheduleListComponent } from './components/schedule-list/schedule-list.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginFormComponent,
+    CourseListComponent,
+    RegisterFormComponent,
+    ScheduleListComponent,
+    
   ],
   imports: [
     BrowserModule,
